e030887c7fb08a423a8a5fa561cffe507f3fc4c0
cucurbitapepo <anedav@yandex.ru>
2023-10-10 23:08:15 +0300

    fixed fixes

--- mat_prac_labs/lab_1_7/functions.c
+++ mat_prac_labs/lab_1_7/functions.c
@@ -91,22 +91,8 @@ int write_with_lowering(FILE** input, FILE** output, int* c)
 
 char digit(int modulo)
 {
-    switch (modulo)
-    {
-        case 0: return '0';        case 1: return '1';        case 2: return '2';
-        case 3: return '3';        case 4: return '4';        case 5: return '5';
-        case 6: return '6';        case 7: return '7';        case 8: return '8';
-        case 9: return '9';        case 10: return 'A';        case 11: return 'B';
-        case 12: return 'C';        case 13: return 'D';        case 14: return 'E';
-        case 15: return 'F';        case 16: return 'G';        case 17: return 'H';
-        case 18: return 'I';        case 19: return 'J';        case 20: return 'K';
-        case 21: return 'L';        case 22: return 'M';        case 23: return 'N';
-        case 24: return 'O';        case 25: return 'P';        case 26: return 'Q';
-        case 27: return 'R';        case 28: return 'S';        case 29: return 'T';
-        case 30: return 'U';        case 31: return 'V';        case 32: return 'W';
-        case 33: return 'X';        case 34: return 'Y';        case 35: return 'Z';
-        default: return 0;
-    }
+    if(modulo > 9) return (modulo + 'A' - 10);
+    return (modulo + '0');
 }
 
 int to_system(int value, char** destination, int base)
