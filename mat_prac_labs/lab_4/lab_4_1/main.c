89ae6556b32b2696c4e66cd03094670ed1f5779a
cucurbitapepo <anedav@yandex.ru>
2023-12-22 15:28:58 +0300

    added lab4

---
+++ mat_prac_labs/lab_4/lab_4_1/main.c
@@ -0,0 +1,494 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define SIZE_HASH 128
+
+#define EPSILON 1e-6
+
+enum errors
+{
+    OK,
+    INVALID_INPUT,
+    INVALID_MEMORY,
+    ERROR_OPEN_FILE,
+    DUBLICATE_KEY
+};
+
+typedef struct Ht_item
+{
+    int hash;
+    char * key;
+    char * value;
+    struct Ht_item * next;
+} Ht_item;
+
+typedef struct Hash_Table
+{
+    Ht_item ** items;
+    int size;
+} Hash_Table;
+
+Hash_Table * create_table(int size_hash)
+{
+    Hash_Table * table = (Hash_Table *) malloc(sizeof(Hash_Table));
+    if(table == NULL)
+    {
+        return NULL;
+    }
+    table->size = size_hash;
+
+    table->items = (Ht_item**) calloc(size_hash, sizeof(Ht_item*));
+    if(table->items == NULL)
+    {
+        free(table);
+        return NULL;
+    }
+    return table;
+}
+
+int hash_function(char* key)
+{
+    int num = 0;
+    int len = strlen(key);
+    for (int i = 0; i < len; i++) {
+        if (key[i] >= '0' && key[i] <= '9')
+            num = num*62 + key[i] - '0';
+        else if (key[i] >= 'A' && key[i] <= 'Z')
+            num = num*62 + key[i] - 'A' + 10;
+        else if (key[i] >= 'a' && key[i] <= 'z')
+            num = num*62 + key[i] - 'a' + 36;
+    }
+    return num;
+}
+
+Ht_item * create_item(char* key, char* value)
+{
+    Ht_item * item = (Ht_item *)malloc(sizeof(Ht_item));
+    if(item == NULL)
+    {
+        return NULL;
+    }
+
+    item->key = (char *) malloc((strlen(key) + 1) * sizeof(char));
+    if(item->key == NULL)
+    {
+        free(item);
+        return NULL;
+    }
+    strcpy(item->key, key);
+
+    item->value = (char *) malloc((strlen(value) + 1) * sizeof(char));
+    if(item->value == NULL)
+    {
+        free(item->key);
+        free(item);
+        return NULL;
+    }
+    strcpy(item->value, value);
+
+//    item->hash = hash_function(key);
+    item->next = NULL;
+    item->hash = 0;
+    return item;
+}
+
+void destroy_item(Ht_item* item)
+{
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+Hash_Table * destroy_hashtable(Hash_Table * table)
+{
+    for(int i = 0; i < table->size; ++i)
+    {
+        Ht_item* item = table->items[i];
+        while(item != NULL)
+        {
+            Ht_item* next = item->next;
+            destroy_item(item);
+            item = next;
+        }
+    }
+    free(table->items);
+    free(table);
+    return NULL;
+}
+
+void insert(Hash_Table* table, char* key, char* value, int hash)
+{
+//    int hash = hash_function(key);
+    int index = hash % table->size;
+
+    Ht_item * item = create_item(key, value);
+    item->hash = hash;
+
+    if (table->items[index] == NULL)
+    {
+        table->items[index] = item;
+    }
+    else
+    {
+        Ht_item* current = table->items[index];
+        while (current->next != NULL)
+        {
+            if(strcmp(current->key, item->key) == 0)
+            {
+                return;
+            }
+            current = current->next;
+        }
+        if(strcmp(current->key, item->key) == 0)
+        {
+            return;
+        }
+        current->next = item;
+    }
+}
+
+void print_hashtable(Hash_Table *table)
+{
+    for (int i = 0; i < table->size; ++i)
+    {
+        if(table->items[i])
+        {
+            printf("Bucket %d:\n", i);
+
+            Ht_item *current = table->items[i];
+            while (current != NULL)
+            {
+                printf("  Key: '%s', Value: '%s'\n", current->key, current->value);
+                current = current->next;
+            }
+            printf("\n");
+        }
+    }
+}
+
+int read_str(FILE * input, char ** buffer, int * capacity, char * c)
+{
+    int idx = 0;
+    *c = fgetc(input);
+    while(*c != ' ' && *c != '\n' && *c != '\t' && *c != EOF)
+    {
+        (*buffer)[idx++] = *c;
+        if(*capacity == idx)
+        {
+            *capacity *= 2;
+            char * for_realloc = (char *) realloc(*buffer, *capacity);
+            if(for_realloc == NULL)
+            {
+                return INVALID_MEMORY;
+            }
+            *buffer = for_realloc;
+        }
+        *c = fgetc(input);
+    }
+    (*buffer)[idx] = '\0';
+    return OK;
+}
+
+int read_define(FILE * input, Hash_Table * table, char ** str, char * c, int * capacity_str)
+{
+    int capacity_key = 20, capacity_value = 20;
+    char * key = (char *)malloc(capacity_key * sizeof(char));
+    if(key == NULL)
+    {
+        return INVALID_MEMORY;
+    }
+    char * value = (char *)malloc(capacity_value * sizeof(char));
+    if(value == NULL)
+    {
+        free(key);
+        return INVALID_MEMORY;
+    }
+
+    while(!feof(input))
+    {
+        if(read_str(input, str, capacity_str, c) != OK)
+        {
+            free(key);
+            free(value);
+            return INVALID_MEMORY;
+        }
+        if(strcmp("#define", *str) == 0)
+        {
+            if (read_str(input, &key, &capacity_key, c) != OK)
+            {
+                free(key);
+                free(value);
+                return INVALID_MEMORY;
+            }
+            int idx = 0;
+            *c = fgetc(input);
+            while(*c != '\n')
+            {
+                (value)[idx++] = *c;
+                if(capacity_value == idx)
+                {
+                    capacity_value *= 2;
+                    char * for_realloc = (char *) realloc(value, capacity_value);
+                    if(for_realloc == NULL)
+                    {
+                        free(key);
+                        free(value);
+                        return INVALID_MEMORY;
+                    }
+                    value = for_realloc;
+                }
+                *c = fgetc(input);
+            }
+            value[idx] = '\0';
+            insert(table, key, value, hash_function(key));
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    free(key);
+    free(value);
+    return OK;
+}
+
+int check_needs_rebuilt(Hash_Table * table)
+{
+    int min = INT_MAX;
+    int max = 0;
+    for (int i = 0; i < table->size; i++)
+    {
+        int current_len = 0;
+        Ht_item* item = table->items[i];
+        if(item == NULL) continue;
+        while (item != NULL)
+        {
+            current_len++;
+            item = item->next;
+        }
+        if (current_len < min)
+        {
+            min = current_len;
+        }
+        if (current_len > max)
+        {
+            max = current_len;
+        }
+    }
+    printf("min collision:%d\nmax collision:%d\n", min, max);
+    double res = (double)max / min;
+    printf("%lf\n", res);
+    if(res - 2 > EPSILON)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int is_prime(int num)
+{
+    if(num % 2 == 0)
+    {
+        return 0;
+    }
+    for(int i = 3; i < num / 2; i += 2)
+    {
+        if(num % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void rebuild_hashtable(Hash_Table * table)
+{
+    int old_size = table->size;
+    Ht_item ** old = table->items;
+    int new_size = table->size * 2;
+    for(; !is_prime(new_size); ++new_size);
+    table->size = new_size;
+
+    table->items = (Ht_item **)calloc(new_size, sizeof(Ht_item*));
+
+    for(int i = 0; i < old_size; ++i)
+    {
+        Ht_item* item = old[i];
+        while (item)
+        {
+            insert(table, item->key, item->value, item->hash);
+            Ht_item* old_item = item;
+            item = item->next;
+            destroy_item(old_item);
+        }
+    }
+    free(old);
+}
+
+char* ht_search(const Hash_Table* table, char* key)
+{
+    int index = hash_function(key) % table->size;
+    Ht_item* item = table->items[index];
+
+    if (item != NULL)
+    {
+        if (strcmp(item->key, key) == 0)
+            return item->value;
+    }
+
+    return NULL;
+}
+
+int replace_text(const Hash_Table * table, FILE * input, FILE * output,  char ** str, char * c, int * capacity_str)
+{
+    int flag_stop = 0;
+    while(!flag_stop)
+    {
+        char * s = ht_search(table, *str);
+        if(s)
+        {
+            if(*c == EOF)
+            {
+                flag_stop = 1;
+                fprintf(output,"%s", s);
+            }
+            else fprintf(output,"%s%c", s, *c);
+        }
+        else
+        {
+            if(*c == EOF)
+            {
+                flag_stop = 1;
+                fprintf(output,"%s", *str);
+            }
+            else fprintf(output,"%s%c", *str, *c);
+        }
+        if(read_str(input, str, capacity_str, c) != OK)
+        {
+            return INVALID_MEMORY;
+        }
+    }
+    return OK;
+}
+
+void replace_file(FILE * input, FILE * output, char * buffer, int size)
+{
+    while (!feof(input))
+    {
+        fgets(buffer, size - 1, input);
+        fprintf(output, "%s", buffer);
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return INVALID_INPUT;
+    }
+
+    FILE * filename = fopen(argv[1], "r");
+    if(filename == NULL)
+    {
+        printf("Error opening file %s\n", argv[1]);
+        return ERROR_OPEN_FILE;
+    }
+
+    FILE * temp = fopen("temp.txt", "w");
+    if(temp == NULL)
+    {
+        printf("Error opening file\n");
+        return ERROR_OPEN_FILE;
+    }
+
+    Hash_Table * table = create_table(SIZE_HASH);
+    if(table == NULL)
+    {
+        printf("memory allocation error\n");
+        fclose(filename);
+        return INVALID_MEMORY;
+    }
+
+    int capacity = 20;
+    char c;
+    char * buffer = (char *)malloc(capacity * sizeof(char));
+    if(buffer == NULL)
+    {
+        destroy_hashtable(table);
+        fclose(filename);
+        fclose(temp);
+        printf("memory allocation error\n");
+        return INVALID_MEMORY;
+    }
+
+    if(read_define(filename, table, &buffer, &c, &capacity) != OK)
+    {
+        destroy_hashtable(table);
+        fclose(filename);
+        fclose(temp);
+        free(buffer);
+        printf("memory allocation error\n");
+        return INVALID_MEMORY;
+    }
+    printf("hash table created\n");
+    print_hashtable(table);
+
+    if(check_needs_rebuilt(table))
+    {
+        printf("hash table after rebuild\n");
+        rebuild_hashtable(table);
+        while(check_needs_rebuilt(table))
+        {
+            rebuild_hashtable(table);
+        }
+        printf("new size: %d\n", table->size);
+        print_hashtable(table);
+    }
+    else
+    {
+        printf("hash table is not rebuilt\n");
+    }
+
+    if(replace_text(table, filename, temp, &buffer, &c, &capacity) != OK)
+    {
+        free(buffer);
+        destroy_hashtable(table);
+        fclose(filename);
+        fclose(temp);
+        printf("memory allocation error\n");
+        return INVALID_MEMORY;
+    }
+
+    fclose(filename);
+    fclose(temp);
+
+    filename = fopen(argv[1], "w");
+    if(filename == NULL)
+    {
+        printf("Error opening file %s\n", argv[1]);
+        return ERROR_OPEN_FILE;
+    }
+
+    temp = fopen("temp.txt", "r");
+    if(temp == NULL)
+    {
+        printf("Error opening file\n");
+        return ERROR_OPEN_FILE;
+    }
+
+    replace_file(temp, filename, buffer, capacity);
+    printf("replacing <def_name> with <value> is done\n");
+
+    free(buffer);
+    destroy_hashtable(table);
+    fclose(filename);
+    fclose(temp);
+    return 0;
+}
---
+++ mat_prac_labs/lab_4/lab_4_7/main.c
@@ -0,0 +1,527 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include <ctype.h>
+
+
+#define SIZE_CAPACITY 20
+
+enum errors
+{
+    OK,
+    INVALID_INPUT,
+    INVALID_MEMORY,
+    ERROR_OPEN_FILE,
+    OVERFLOW_ERROR,
+    DIVISION_BY_ZERO
+};
+
+typedef struct MemoryCell
+{
+    char * name;
+    long int value;
+} MemoryCell;
+
+typedef struct Variables
+{
+    long int capacity;
+    long int size;
+    MemoryCell ** array;
+} Variables;
+
+Variables * create_variables()
+{
+    Variables * var = (Variables *) malloc(sizeof(Variables));
+    if(var == NULL)
+    {
+        return NULL;
+    }
+
+    var->capacity = SIZE_CAPACITY;
+    var->size = 0;
+
+    var->array = (MemoryCell **) malloc(sizeof(MemoryCell *) * SIZE_CAPACITY);
+    if(var->array == NULL)
+    {
+        free(var);
+        return NULL;
+    }
+
+    return var;
+}
+
+MemoryCell * create_mc(char * name, long int num)
+{
+    MemoryCell * var = (MemoryCell *) malloc(sizeof(MemoryCell));
+    if(var == NULL)
+    {
+        return NULL;
+    }
+
+    long int len_name = strlen(name);
+
+    var->name = (char *) malloc((len_name + 1) * sizeof(char ));
+    if(var->name == NULL)
+    {
+        free(var);
+        return NULL;
+    }
+
+    strcpy(var->name, name);
+    var->value = num;
+
+    return var;
+}
+
+int compare(const void* a, const void* b)
+{
+    MemoryCell* mc1 = *(MemoryCell**)a;
+    MemoryCell* mc2 = *(MemoryCell**)b;
+    return strcmp(mc1->name, mc2->name);
+}
+
+MemoryCell* search_variable(Variables* vars, char* name)
+{
+    int left = 0;
+    int right = vars->size - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        int res = strcmp(vars->array[mid]->name, name);
+        if (res == 0)
+        {
+            return vars->array[mid];
+        }
+        if (res < 0)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return NULL;
+}
+
+enum errors add_variable(Variables* vars, char* name, long int num)
+{
+    if (vars->size == vars->capacity) {
+        vars->capacity *= 2;
+        MemoryCell** for_realloc = (MemoryCell**)realloc(vars->array, vars->capacity * sizeof(MemoryCell*));
+        if(for_realloc == NULL)
+        {
+            return INVALID_MEMORY;
+        }
+        vars->array = for_realloc;
+    }
+    MemoryCell* new_var = create_mc(name, num);
+    if(new_var == NULL)
+    {
+        return INVALID_MEMORY;
+    }
+    vars->array[vars->size++] = new_var;
+    qsort(vars->array, vars->size, sizeof(MemoryCell*), compare);
+    return OK;
+}
+
+void destroy_mc(MemoryCell * var)
+{
+    free(var->name);
+    free(var);
+}
+
+void destroy_variables(Variables * vars)
+{
+    for (int i = 0; i < vars->size; ++i)
+    {
+        destroy_mc(vars->array[i]);
+    }
+    free(vars->array);
+    free(vars);
+}
+
+int read_str(FILE * input, char ** str, int * capacity)
+{
+    char c = fgetc(input);
+    int idx = 0;
+    while (c != ';')
+    {
+        (*str)[idx++] = c;
+        if (idx == *capacity)
+        {
+            *capacity *= 2;
+            char * for_realloc = (char *)realloc(*str, *capacity);
+            if (for_realloc == NULL)
+            {
+                return INVALID_MEMORY;
+            }
+            *str = for_realloc;
+        }
+        c = fgetc(input);
+    }
+    (*str)[idx] = '\0';
+    c = fgetc(input);
+    if (c == EOF)
+    {
+        return OK;
+    }
+    return OK;
+}
+
+int convert_str_to_int (const char *str, long int * result, int base)
+{
+    char *endptr;
+    long num = strtol(str, &endptr, base);
+
+    if (errno == ERANGE && (num == LONG_MAX || num == LONG_MIN))
+    {
+        return INVALID_INPUT;
+    }
+    else if (errno != 0 && num == 0)
+    {
+        return INVALID_INPUT;
+    }
+    else if (*endptr != '\0')
+    {
+        return INVALID_INPUT;
+    }
+
+    *result = num;
+    return OK;
+}
+
+int check_is_digit(char * str)
+{
+    size_t i = 0;
+    for(; i < strlen(str); ++i)
+    {
+        if(!isdigit(str[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int get_num(MemoryCell * var, long int * num, char * buffer, int * flag_negative)
+{
+    if(var == NULL)
+    {
+        int err = convert_str_to_int(buffer, num, 10);
+        if(err == INVALID_INPUT)
+        {
+            if(check_is_digit(buffer))
+            {
+                return OVERFLOW_ERROR;
+            }
+            else
+            {
+                return INVALID_INPUT;
+            }
+        }
+    }
+    else
+    {
+        *num = var->value;
+    }
+
+    if(*flag_negative)
+    {
+        (*num) *= -1;
+        *flag_negative = 0;
+    }
+    return OK;
+}
+
+int get_res(char operation, long int num1, long int num2, long int * res)
+{
+    if(operation == '+')
+    {
+        if (num1 > 0 && num2 > LONG_MAX - num1) {
+            return OVERFLOW_ERROR;
+        } else if (num1 < 0 && num2 < LONG_MIN - num1) {
+            return OVERFLOW_ERROR;
+        }
+        *res = num1 + num2;
+    }
+    else if(operation == '-')
+    {
+        if (num1 > 0 && num2 < LONG_MIN + num1) {
+            return OVERFLOW_ERROR;
+        } else if (num1 < 0 && num2 > LONG_MAX + num1) {
+            return OVERFLOW_ERROR;
+        }
+        *res = num1 - num2;
+    }
+    else if (operation == '*')
+    {
+        if (num1 > 0 && num2 > LONG_MAX / num1) {
+            return OVERFLOW_ERROR;
+        } else if (num1 < 0 && num2 < LONG_MIN / num1) {
+            return OVERFLOW_ERROR;
+        }
+        *res = num1 * num2;
+    }
+    else if(operation == '/')
+    {
+        if(num2 == 0)
+        {
+            return DIVISION_BY_ZERO;
+        }
+        *res = num1 / num2;
+    }
+    else if(operation == '%')
+    {
+        if(num2 == 0)
+        {
+            return DIVISION_BY_ZERO;
+        }
+        *res = num1 % num2;
+    }
+    return OK;
+}
+
+int execution_instructions(char * str, Variables * vars, const char * operations)
+{
+    int err;
+    int len_str = strlen(str);
+    char * buffer = (char *) malloc((len_str + 1) * sizeof(char));
+    if(buffer == NULL)
+        return INVALID_MEMORY;
+
+    int idx = 0;
+    for(;str[idx] != '='; idx++)
+    {
+        buffer[idx] = str[idx];
+    }
+    buffer[idx++] = '\0';
+
+    MemoryCell * result = search_variable(vars, buffer);
+
+    int flag_negative = 0;
+
+    if(str[idx] == '~')
+    {
+        flag_negative = 1;
+        idx++;
+    }
+
+    int i = 0;
+    while (str[idx] != '\0' && strrchr(operations, str[idx]) == NULL)
+    {
+        buffer[i++] = str[idx++];
+    }
+    buffer[i] = '\0';
+
+    long int num1;
+    MemoryCell * var1 = search_variable(vars, buffer);
+
+    err = get_num(var1, &num1, buffer, &flag_negative);
+    if(err != OK)
+    {
+        free(buffer);
+        return err;
+    }
+
+    long int res = 0;
+
+    if (str[idx] == '\0')
+    {
+        res = num1;
+    }
+    else
+    {
+
+        char operation = str[idx++];
+
+        if(str[idx] == '~')
+        {
+            flag_negative = 1;
+            idx++;
+        }
+
+        for(i = 0; str[idx] != '\0'; i++, idx++)
+        {
+            buffer[i] = str[idx];
+        }
+        buffer[i] = '\0';
+
+        long int num2;
+        MemoryCell * var2 = search_variable(vars, buffer);
+
+        err = get_num(var2, &num2, buffer, &flag_negative);
+        if(err != OK)
+        {
+            free(buffer);
+            return err;
+        }
+
+        err = get_res(operation, num1, num2, &res);
+        if(err != OK)
+        {
+            free(buffer);
+            return err;
+        }
+    }
+
+
+    if(result == NULL)
+    {
+        int j = 0;
+        for(;str[j] != '='; j++)
+        {
+            buffer[j] = str[j];
+        }
+        buffer[j] = '\0';
+
+        enum errors err = add_variable(vars, buffer, res);
+        if(err == INVALID_MEMORY)
+        {
+            free(buffer);
+            return INVALID_MEMORY;
+        }
+    }
+    else
+    {
+        result->value = res;
+    }
+
+    free(buffer);
+    return OK;
+}
+
+
+void print_all(Variables * vars)
+{
+    for(int i = 0; i < vars->size; ++i)
+    {
+        MemoryCell * vvv = vars->array[i];
+        printf("%s: %ld\n", vvv->name, vvv->value);
+    }
+    printf("%ld\n", vars->size);
+}
+
+int main(int argc, char * argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return INVALID_INPUT;
+    }
+
+    FILE * filename = fopen(argv[1], "r");
+    if(filename == NULL)
+    {
+        printf("Error opening file %s\n", argv[1]);
+        return ERROR_OPEN_FILE;
+    }
+
+    Variables * vars = create_variables();
+    if(vars == NULL)
+    {
+        printf("memory allocation error\n");
+        fclose(filename);
+        return INVALID_MEMORY;
+    }
+
+    int capacity = SIZE_CAPACITY;
+    char * buffer = (char *) malloc(capacity * sizeof(char));
+    if(buffer == NULL)
+    {
+        printf("memory allocation error\n");
+        fclose(filename);
+        destroy_variables(vars);
+        return INVALID_MEMORY;
+    }
+
+
+    char operations[6] = "+-*/%";
+    while (!feof(filename))
+    {
+        if(read_str(filename, &buffer, &capacity) != OK)
+        {
+            printf("memory allocation error\n");
+            fclose(filename);
+            free(buffer);
+            destroy_variables(vars);
+            return INVALID_MEMORY;
+        }
+        if(strcmp("print", buffer) == 0)
+        {
+            printf("print all\n");
+            print_all(vars);
+        }
+        else if(strstr(buffer, "print") != NULL)
+        {
+            printf("print var\n");
+            char * p = strtok(buffer, " ");
+            p = strtok(NULL, " ");
+            if(p == NULL)
+            {
+                printf("unknown variable\n");
+                destroy_variables(vars);
+                fclose(filename);
+                free(buffer);
+                return INVALID_INPUT;
+            }
+
+            MemoryCell * var = search_variable(vars, p);
+            if(var != NULL)
+            {
+                printf("%s: %ld\n", var->name, var->value);
+            }
+            else
+            {
+                printf("unknown variable\n");
+                destroy_variables(vars);
+                fclose(filename);
+                free(buffer);
+                return INVALID_INPUT;
+            }
+        }
+        else
+        {
+            enum errors err=execution_instructions(buffer, vars, operations);
+            if(err == INVALID_INPUT)
+            {
+                printf("unknown variable\n");
+                destroy_variables(vars);
+                fclose(filename);
+                free(buffer);
+                return INVALID_INPUT;
+            }
+            else if(err == INVALID_MEMORY)
+            {
+                printf("memory allocation error\n");
+                fclose(filename);
+                free(buffer);
+                destroy_variables(vars);
+                return INVALID_MEMORY;
+            }
+            else if(err == OVERFLOW_ERROR)
+            {
+                printf("overflow occurred\n");
+                fclose(filename);
+                free(buffer);
+                destroy_variables(vars);
+                return OVERFLOW_ERROR;
+            }
+            else if(err == DIVISION_BY_ZERO)
+            {
+                printf("division by zero\n");
+                fclose(filename);
+                free(buffer);
+                destroy_variables(vars);
+                return DIVISION_BY_ZERO;
+            }
+        }
+    }
+
+    destroy_variables(vars);
+    fclose(filename);
+    free(buffer);
+    return 0;
+}
